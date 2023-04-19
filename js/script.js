ff4d1cd8d035396891c95a5e4c687e1ce808457f
Angel <angelica.e.madera@gmail.com>
2023-04-18 21:04:57 -0500

    added map markers

--- js/script.js
+++ js/script.js
@@ -17,4 +17,31 @@ async function initMap() {
     center: chicago,
     mapId: "DEMO_MAP_ID",
   });
+
+//array consisting of different location coordinates
+  const locations = [
+    [{ lat: 41.8349, lng: -87.6270 }, "Illinois Institute of Technology", "assets/favicon.ico"],
+    [{ lat: 42.0565, lng: -87.6753  }, "Northwestern University", "assets/northwestern-logo.jpeg"],
+    [{ lat: 42.0453, lng: -87.6811 }, "Evanson McGaw YMCA", "assets/ymca-logo.png"],
+    [{ lat: 41.9811, lng: -87.8397 }, "UScellular Business Office", "assets/uscc-logo.png"],
+    [{ lat: 41.8816, lng: -87.6301 }, "J.P. Morgan Chase", "assets/jp-morgan-chase-logo.png"]
+  ];
+
+  //for each location, create a marker
+  locations.forEach(([position, title, icon], i) => {
+    const marker = new google.maps.Marker({
+      position,
+      map: map,
+      icon: icon,
+      title: `${title}`,
+      optimized: false,
+    });
+
+    // Add a click listener for each marker, and set up the info window.
+    marker.addListener("click", () => {
+      infoWindow.close();
+      infoWindow.setContent(marker.getTitle());
+      infoWindow.open(marker.getMap(), marker);
+    });
+  });
 }
